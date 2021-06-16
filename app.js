7ec2f83ca4c8e2d2644bf0e28c64f90c645b663f
Zain Khan <zain.khaan147@gmail.com>
2021-06-16 22:54:24 +0500

    added heal player fuctionality

--- app.js
+++ app.js
@@ -38,6 +38,16 @@ const app = Vue.createApp({
       this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
   },
 });
 
