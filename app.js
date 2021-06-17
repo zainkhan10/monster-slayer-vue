951cfd8be0799c1216cb91d84d1c2f7618ddb0be
Zain Khan <zain.khaan147@gmail.com>
2021-06-17 18:19:55 +0500

    Implement the core functionality

--- app.js
+++ app.js
@@ -13,9 +13,15 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
@@ -39,6 +45,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.winner = null;
+      this.currentRound = 0;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
@@ -64,6 +76,9 @@ const app = Vue.createApp({
         this.playerHealth += healValue;
       }
       this.attackPlayer();
+    },
+    surrender() {
+      this.winner = "monster";
     },
   },
 });
