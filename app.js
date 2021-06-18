455f535fa53c1549122ef7ee78ac288ebbedb188
Zain Khan <zain.khaan147@gmail.com>
2021-06-18 20:38:09 +0500

    Added battle logs

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessages: [],
     };
   },
   computed: {
@@ -50,21 +51,25 @@ const app = Vue.createApp({
       this.currentRound = 0;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.logMessages = [];
     },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer();
     },
     attackPlayer() {
-      const attackValue = getRandomValue(8, 15);
+      const attackValue = getRandomValue(8, 15);    
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue)
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 20);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue)
       this.attackPlayer();
     },
     healPlayer() {
@@ -75,10 +80,18 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage('player', 'heal', healValue)
       this.attackPlayer();
     },
     surrender() {
       this.winner = "monster";
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 });
